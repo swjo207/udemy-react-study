bbd329b00c1899f795547b44b02c1559a01a7def
swjo207 <swjo207@gmail.com>
2023-03-18 05:11:18 +0900

    회원가입 컴포넌트, 폼 입력 컴포넌트, 버튼 컴포넌트

--- crwn-clothing/src/components/category-item/sign-up-form/sign-up-form.component.jsx
+++ crwn-clothing/src/components/category-item/sign-up-form/sign-up-form.component.jsx
@@ -4,6 +4,8 @@ import {
 } from "../../../utils/firebase/firebase.utils";
 import { useState } from "react";
 import FormInput from "../../form-input/form-input.component";
+import './sign-up-form.styles.scss'
+import Button from "../../button/button.component";
 
 const defaultFormFields = {
   displayName: "",
@@ -53,8 +55,9 @@ const SignUpForm = () => {
 
   return (
     <>
-      <div>
-        <h1> Sign up with your email and password. </h1>
+      <div className="sign-up-container">
+        <h2> Don't have an account? </h2>
+        <span> Sign up with your email and password. </span>
         <form onSubmit={handleSubmit}>
           <FormInput
             label="Display Name"
@@ -92,7 +95,7 @@ const SignUpForm = () => {
             value={confirmPassword}
           />
 
-          <button type="submit"> Sign Up </button>
+          <Button buttonType="default" type="submit"> Sign Up </Button>
         </form>
       </div>
     </>
