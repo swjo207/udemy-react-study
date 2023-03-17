711097c8ab1d6a9fcdc87c8a0d68d1677dd3a9de
swjo207 <swjo207@gmail.com>
2023-03-18 01:48:11 +0900

    navigation bar 라우터 만들기

---
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,27 @@
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+import './navigation.sytles.scss'
+
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+       
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      
+      </div>
+
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
