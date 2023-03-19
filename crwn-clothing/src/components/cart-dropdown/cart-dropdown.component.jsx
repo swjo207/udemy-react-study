0db2ac7c460ce42de91af64253f831a5cf73b4e8
swjo207 <swjo207@gmail.com>
2023-03-19 22:21:04 +0900

    카트 화면

--- crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,15 @@
 import './cart-dropdown.styles.scss'; 
 import Button from '../button/button.component'; 
 import { useContext } from "react";
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.context'; 
 import CartItem from '../cart-item/cart-item.component';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext); 
+  const navigate = useNavigate(); 
+
+  const goToCheckoutHandler = () => navigate('/checkout'); 
 
   return (
     <div className='cart-dropdown-container'>
@@ -14,7 +18,7 @@ const CartDropdown = () => {
           return <CartItem key={item.id} cartItem={item} />
         }
         )}
-        <Button>Go to checkout.. </Button>
+        <Button onClick={goToCheckoutHandler}>Go to checkout.. </Button>
     </div>
   )
 }
