11bb25d78b15150a0aa587b02c7255e4f567c698
swjo207 <swjo207@gmail.com>
2023-03-20 01:03:23 +0900

    그룹 카테고리 화면 변경

--- crwn-clothing/src/routes/home/home.component.jsx
+++ crwn-clothing/src/routes/home/home.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
-import Directory from '../../components/category-item/directory/directory.component';
+import Directory from '../../components/directory/directory.component';
 
 const Home = () => {
     const categories = [
