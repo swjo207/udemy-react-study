43c91aad02622f87d4b428ff954384ca35e82634
swjo207 <swjo207@gmail.com>
2023-03-23 01:03:33 +0900

    redux-persist,

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import App from './App';
-import { store } from './store/store';
+import App from "./App";
+import { store, persistor } from "./store/store";
 
-import './index.scss';
+import "./index.scss";
 
-const rootElement = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
 
 rootElement.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-            <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
